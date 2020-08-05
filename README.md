e72bc3675380a0b5837eab19c135f20857aeb16c
ylor <rolyreyes@me.com>
2020-08-04 20:52:15 -0400

    Push 1.1 + 1.2 source

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # New Tab Home
+![icon](src/icon@2x.png)
+
 Set your Firefox configured homepage to new tab page. Because it's what makes sense.
 
-Inspiration: [New Tab Override](https://addons.mozilla.org/en-US/firefox/addon/new-tab-override/)
+## Changelog
+- 1.2
+  - Added icon
+- 1.1
+  - Add preference for cursor focus when new tab has loaded
+    - Options are `Website` or `Address Bar`
+- 1.0
+  - Initial release
 
-## To do
-* Create icon
-* Create preference for focus in address bar or webpage
+## Acknowledgements
+This extension would not have been possible without:
+* Mozilla for [Firefox](https://www.mozilla.org/firefox/), [Fira Sans](https://mozilla.github.io/Fira/), [Firefox Design resources](https://design.firefox.com/photon/), and their [excellent WebExtension documentation](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions)
+  * Firefox is the only major cross-platform browser left that does not run on WebKit/Blink
+  * Firefox is the only major cross-platform browser that I don't feel horrible about using
+* [Visual Studio Code](https://code.visualstudio.com/) for writing code
+* [Figma](https://www.figma.com/) for icon design
+* [New Tab Override](https://addons.mozilla.org/en-US/firefox/addon/new-tab-override/) for inspiration
--- src/newtab.js
+++ src/newtab.js
@@ -14,14 +14,15 @@ async function getHome() {
 
     if (home.startsWith("about:")) {
       //exit early if home begins with "about:"
-      console.error("about:");
-      return "about:blank";
+      console.log("Currently configured homepage is " + result.value);
+      home = "about:blank";
     } else if (!home.startsWith("http")) {
+      // if configured homepage doesn't begin with http(s) prepend it
       home = "http://" + home;
     }
     return home;
   } else {
-    console.log("Unable to retrieve configured homepage.");
+    console.error("Unable to retrieve configured homepage.");
     return "about:blank";
   }
 }
@@ -31,24 +32,21 @@ async function getHome() {
  */
 
 async function newTabHome() {
-  let home = await getHome();
-  const focus = true;
+  const home = await getHome();
+  const focus = await browser.storage.local
+    .get({ focus: "website" })
+    .then((data) => data.focus);
 
   await browser.tabs.getCurrent((tab) => {
     const tabId = tab.id;
 
-    if (focus) {
-      browser.tabs.create(
-        { url: home, cookieStoreId: tab.cookieStoreId },
-        (tab) => {
-          // cookieStoreId required to support the container tabs
-          browser.tabs.remove(tabId);
-        }
-      );
+    if (focus === "addressbar") {
+      browser.tabs.update(tabId, { url: home, loadReplace: true }, () => {});
+      // Nothing to do
+      // Required for browser.history.deleteUrl()
     } else {
-      browser.tabs.update(tabId, { url: home, loadReplace: true }, () => {
-        // Nothing to do
-        // Required for browser.history.deleteUrl() to work
+      browser.tabs.create({ url: home }, (tab) => {
+        browser.tabs.remove(tabId);
       });
     }
   });
